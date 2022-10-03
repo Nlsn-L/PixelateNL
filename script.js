b03ac6456be78bdbc2199c3623d8465f9745a67f
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-10-03 10:34:32 -0500

    got to adding color

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 // Your code here
-const ul = document.getElementById('the-ul')
-const newLi = document.createElement('li')
-ul.appendChild(newLi)
 
 const table = document.getElementsByTagName('table')[0]
 
 function makeRow() {
     const row = document.createElement('tr')
-    for (let i = 0; i < 20; i++){
+    for (let i = 0; i < 20; i++) {
         const td = document.createElement('td')
         row.appendChild(td)
     }
     table.appendChild(row)
 }
 
+makeRow();
+makeRow();
 
-makeRow();
-makeRow();
+const addRowButton = document.getElementById('add-row');
+addRowButton.addEventListener('click', function() {
+    makeRow();
+});
+
+table.addEventListener('click', function (colorize) {
+    console.log('clicked')
+})
